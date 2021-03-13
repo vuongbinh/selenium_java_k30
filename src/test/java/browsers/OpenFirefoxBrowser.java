fda1357c385dde2f1e49b018ece243eed78492ea
vuong_binh <binh.vuonghao@gmail.com>
2021-03-13 23:02:51 +0700

    Update code

--- src/test/java/browsers/OpenFirefoxBrowser.java
+++ src/test/java/browsers/OpenFirefoxBrowser.java
@@ -1,13 +1,11 @@
 package browsers;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import support.Browsers;
 
 public class OpenFirefoxBrowser {
     public static void main(String[] args){
-        //System.setProperty("webdriver.gecko.driver","drivers/geckodriver.exe");
-        WebDriverManager.firefoxdriver().setup();
-        WebDriver driver = new FirefoxDriver();
+        Browsers obj = new Browsers();
+        obj.open("firefox");
+
     }
 }
